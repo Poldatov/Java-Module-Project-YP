92b8135c62beb152cb11b8e793eac07bfcb047f3
Poldatov <78009597+Poldatov@users.noreply.github.com>
2024-02-18 22:17:54 +0300

    Delete src/main/java/Products.java

--- src/main/java/Products.java
+++
@@ -1,9 +0,0 @@
-public class Products {
-    final String name;
-    final double price;
-
-    public Products(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-}
